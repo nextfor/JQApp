ae3c503bd68cf65df175951ff9a5c0e23a603d48
Jojoyel <jolalle974@gmail.com>
2021-07-20 14:02:13 +0200

    flblblblbl

---
+++ app/src/main/java/com/jojo/jojozquizz/SettingsActivity.java
@@ -0,0 +1,193 @@
+package com.jojo.jojozquizz;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+import com.jojo.jojozquizz.model.Question;
+import com.jojo.jojozquizz.tools.QuestionsDatabase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
+
+	private static final String TAG = "SettingsActivity";
+
+	private Context mContext;
+	private SharedPreferences mPreferences;
+
+	private Button mReloadButton, mPrivacyButton, mTermsButton;
+	private RadioButton mFrButton, mEnButton;
+
+	private final String API_URL = "https://nextfor.studio/";
+
+	private MutableLiveData<Integer> LAST_ID;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+
+		mContext = this;
+
+		mPreferences = this.getSharedPreferences("com.jojo.jojozquizz", MODE_PRIVATE);
+
+		mReloadButton = findViewById(R.id.settings_reload_questions);
+		mFrButton = findViewById(R.id.settings_radio_fr);
+		mEnButton = findViewById(R.id.settings_radio_en);
+		mPrivacyButton = findViewById(R.id.privacy_policy);
+		mTermsButton = findViewById(R.id.terms_of_service);
+
+		mReloadButton.setTag(1);
+		mPrivacyButton.setTag(2);
+		mTermsButton.setTag(3);
+		mReloadButton.setOnClickListener(this);
+		mPrivacyButton.setOnClickListener(this);
+		mTermsButton.setOnClickListener(this);
+
+		String langage = mPreferences.getString("langage", "EN");
+		switch (langage) {
+			case "FR":
+				mFrButton.setChecked(true);
+				break;
+			default:
+				mEnButton.setChecked(true);
+				break;
+		}
+
+		LAST_ID = new MutableLiveData<>();
+		LAST_ID.observe(this, new Observer<Integer>() {
+			@Override
+			public void onChanged(Integer integer) {
+				if (QuestionsDatabase.getInstance(mContext).QuestionDAO().getAllQuestions().isEmpty() || integer > QuestionsDatabase.getInstance(mContext).QuestionDAO().getLastQuestion().getId()) {
+					addQuestions(integer);
+				}
+			}
+		});
+	}
+
+	@Override
+	protected void onStop() {
+		String selectedLang = mPreferences.getString("langage", "EN");
+		if (mFrButton.isChecked()) {
+			if (selectedLang.equals("EN")) {
+				QuestionsDatabase.getInstance(mContext).QuestionDAO().deleteTable();
+			}
+			mPreferences.edit().putString("langage", "FR").apply();
+		} else if (mEnButton.isChecked()) {
+			if (selectedLang.equals("FR"))
+				QuestionsDatabase.getInstance(mContext).QuestionDAO().deleteTable();
+			mPreferences.edit().putString("langage", "EN").apply();
+		}
+
+		super.onStop();
+	}
+
+	private void getLastIdFromServer() {
+		String lastIdRoute = "questions/getLastId/";
+		String lang = mPreferences.getString("langage", "EN");
+
+		RequestQueue requestQueue = Volley.newRequestQueue(this);
+		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, API_URL + lastIdRoute + lang, null, new Response.Listener<JSONObject>() {
+			@Override
+			public void onResponse(JSONObject response) {
+				try {
+					LAST_ID.setValue(response.getInt("questionId"));
+				} catch (JSONException ignore) {
+				}
+			}
+		}, new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				Snackbar.make(findViewById(R.id.settings_constraint_layout), getString(R.string.impossible_to_load_questions), Snackbar.LENGTH_LONG).setAction(getString(R.string.all_retry), new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						getLastIdFromServer();
+					}
+				}).show();
+			}
+		});
+		requestQueue.add(jsonObjectRequest);
+	}
+
+	public void addQuestions(int lastId) {
+
+		long lastIdInDatabase;
+		if (QuestionsDatabase.getInstance(this).QuestionDAO().getAllQuestions().isEmpty()) {
+			lastIdInDatabase = 0;
+		} else {
+			lastIdInDatabase = QuestionsDatabase.getInstance(this).QuestionDAO().getLastQuestion().getId() + 1;
+		}
+
+		String apiRoute = "questions/getQuestion/";
+		String lang = mPreferences.getString("langage", "EN");
+
+		RequestQueue requestQueue = Volley.newRequestQueue(this);
+		for (long i = lastIdInDatabase; i < lastId + 1; i++) {
+			String fullRoute = API_URL + apiRoute + lang + "/" + i;
+
+			JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, fullRoute, null, new Response.Listener<JSONObject>() {
+				@Override
+				public void onResponse(JSONObject response) {
+					try {
+						int id = response.getInt("questionId");
+						String q = response.getString("question");
+						String choices = response.getString("choices");
+						int category = response.getInt("category");
+						int difficulty = response.getInt("difficulty");
+						Question question = new Question(id, q, choices, category, difficulty);
+						QuestionsDatabase.getInstance(mContext).QuestionDAO().addQuestion(question);
+					} catch (JSONException ignored) {
+					}
+				}
+			}, null);
+			requestQueue.add(jsonObjectRequest);
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		int buttonTag = (int) v.getTag();
+
+		if (buttonTag == 1) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(getResources().getString(R.string.settings_rewrite_database))
+				.setMessage(getResources().getString(R.string.settings_rewrite_database_confirmation))
+				.setNegativeButton(getResources().getString(R.string.all_cancel), null)
+				.setPositiveButton(getString(R.string.rewrite), new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						QuestionsDatabase.getInstance(mContext).QuestionDAO().deleteTable();
+						getLastIdFromServer();
+					}
+				});
+			builder.show();
+
+		} else if (buttonTag == 2) {
+			Intent launchBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://nextfor.studio/html/jojozquizz/privacy_policy/" + mPreferences.getString("langage", "EN")));
+			startActivity(launchBrowser);
+		} else if (buttonTag == 3) {
+			Intent launchBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://nextfor.studio/html/jojozquizz/terms_of_service/" + mPreferences.getString("langage", "EN")));
+			startActivity(launchBrowser);
+		}
+	}
+}
---
+++ app/src/main/java/com/jojo/jojozquizz/tools/CategoriesAdapter.java
@@ -0,0 +1,83 @@
+package com.jojo.jojozquizz.tools;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jojo.jojozquizz.R;
+
+import java.util.List;
+
+public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.CategoriesViewHolder> {
+
+	private Context mContext;
+	private String[] mCategories;
+	private List<String> mCategoriesSelected;
+
+	private String[] mCategoriesChecked;
+
+	private CategoriesCheckListener mCheckListener;
+
+	public CategoriesAdapter(Context ct, String[] categories, List<String> categoriesSelected) {
+		this.mContext = ct;
+		this.mCategories = categories;
+		this.mCategoriesSelected = categoriesSelected;
+		mCategoriesChecked = this.mCategories;
+	}
+
+	public void changeCategoriesChecked(List<String> newCategories) {
+		mCategoriesSelected = newCategories;
+	}
+
+	@NonNull
+	@Override
+	public CategoriesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		LayoutInflater inflater = LayoutInflater.from(mContext);
+		View view = inflater.inflate(R.layout.categories_recycler_layout, parent, false);
+
+		mCheckListener = (CategoriesCheckListener) mContext;
+		return new CategoriesViewHolder(view);
+	}
+
+	@Override
+	public void onBindViewHolder(@NonNull CategoriesViewHolder holder, int position) {
+		CheckBox currentCheckbox = holder.mCheckBox;
+
+		currentCheckbox.setText(mCategories[position]);
+		currentCheckbox.setButtonTintList(ContextCompat.getColorStateList(mContext, position % 2 == 0 ? R.color.colorPrimary : R.color.colorPrimaryDark));
+		currentCheckbox.setChecked(mCategoriesSelected.contains(currentCheckbox.getText().toString()));
+
+		holder.mCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+			String text = String.valueOf(buttonView.getText());
+			mCheckListener.checkChanged(text, isChecked);
+		});
+	}
+
+	@Override
+	public int getItemCount() {
+		return mCategories.length;
+	}
+
+	public static class CategoriesViewHolder extends RecyclerView.ViewHolder {
+		CheckBox mCheckBox;
+		ConstraintLayout mConstraintLayout;
+
+		public CategoriesViewHolder(@NonNull View v) {
+			super(v);
+
+			mConstraintLayout = v.findViewById(R.id.categories_layout);
+			mCheckBox = v.findViewById(R.id.checkbox);
+		}
+	}
+
+	public interface CategoriesCheckListener {
+		void checkChanged(String category, boolean isChecked);
+	}
+}
