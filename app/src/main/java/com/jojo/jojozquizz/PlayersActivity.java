023a4c1952b18e523a6bf0c51498f66e2f776f8b
Jonathan LALLEMAND <jolalle974@gmail.com>
2021-09-03 01:21:54 +0200

    removed return button in PlayersActivity.java, now belongs to children fragments

--- app/src/main/java/com/jojo/jojozquizz/PlayersActivity.java
+++ app/src/main/java/com/jojo/jojozquizz/PlayersActivity.java
@@ -22,11 +22,9 @@ import com.jojo.jojozquizz.tools.PlayersDatabase;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PlayersActivity extends AppCompatActivity implements NameDialog.NameDialogListener, ClickHandler {
+public class PlayersActivity extends AppCompatActivity {
 
 	private static final String TAG = "PlayersActivity";
-
-	private ImageButton mBackButton;
 
 	ActivityPlayersBinding mBinding;
 
@@ -39,45 +37,10 @@ public class PlayersActivity extends AppCompatActivity implements NameDialog.Nam
 		setContentView(R.layout.activity_players);
 
 		mBinding = DataBindingUtil.setContentView(this, R.layout.activity_players);
-		mBinding.setHandler(this);
-
-		mBackButton = findViewById(R.id.button_back_users_activity);
 
 		mPlayersFragment = new PlayersFragment();
 		mPlayerInformationFragment = new PlayerInformationFragment();
 
 		getSupportFragmentManager().beginTransaction().add(R.id.frameLayoutPlayers, mPlayersFragment).commit();
-	}
-
-	@Override
-	public void onButtonClick(View v) {
-		int id = v.getId();
-
-		if (id == R.id.button_back_users_activity) {
-			finish();
-		}
-	}
-
-	@Override
-	public boolean onLongButtonClick(View v) {
-		return false;
-	}
-
-	@Override
-	public void applyText(String name) {
-		Pattern pattern = Pattern.compile(NameDialog.REGEX);
-		Matcher matcher = pattern.matcher(name);
-		if (!matcher.find()) {
-			NameDialog nameDialog = new NameDialog();
-			nameDialog.setIsNewUser(true);
-			nameDialog.show(getSupportFragmentManager(), "name dialog usersactivity");
-			Toast.makeText(this, getResources().getString(R.string.invalid_name), Toast.LENGTH_SHORT).show();
-		} else {
-			Player player = new Player(name, this);
-			PlayersDatabase.getInstance(this).PlayersDAO().addPlayer(player);
-
-			Player newPlayer = PlayersDatabase.getInstance(this).PlayersDAO().getPlayerFromName(player.getName());
-			this.getSharedPreferences("com.jojo.jojozquizz", Context.MODE_PRIVATE).edit().putInt("currentUserId", newPlayer.getId()).apply();
-		}
 	}
 }
