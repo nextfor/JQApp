90d24124165aa55cbb7f03159f425ffb4919baf8
Jonathan LALLEMAND <jolalle974@gmail.com>
2021-09-03 01:20:50 +0200

    Reformat code

--- app/src/main/java/com/jojo/jojozquizz/tools/PlayersAdapter.java
+++ app/src/main/java/com/jojo/jojozquizz/tools/PlayersAdapter.java
@@ -1,7 +1,6 @@
 package com.jojo.jojozquizz.tools;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +8,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.jojo.jojozquizz.PlayersInformationActivity;
 import com.jojo.jojozquizz.R;
 import com.jojo.jojozquizz.model.Player;
 
