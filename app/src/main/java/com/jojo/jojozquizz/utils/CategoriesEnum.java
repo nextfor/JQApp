de098ac9fec683cc2053a1cf3ddb6bed5cc6a2c2
Jonathan LALLEMAND <jolalle974@gmail.com>
2021-11-21 12:02:27 +0100

    Created Enum of categories

---
+++ app/src/main/java/com/jojo/jojozquizz/utils/CategoriesEnum.java
@@ -0,0 +1,37 @@
+package com.jojo.jojozquizz.utils;
+
+import com.jojo.jojozquizz.R;
+
+public enum CategoriesEnum {
+	AERONAUTICS(0, R.string.category_aeronautics),
+	OTHER(1, R.string.category_other),
+	ANIMALS(2, R.string.category_animals),
+	ANIMEMANGA(3, R.string.category_animemanga),
+	AUTOMOTIVE(4, R.string.category_automotive),
+	COMICS(5, R.string.category_comics),
+	CELEBRITIES(6, R.string.category_celebrities),
+	CINEMA(7, R.string.category_cinema),
+	QUOTES(8, R.string.category_quotes),
+	COOKING(9, R.string.category_cooking),
+	ECONOMY(10, R.string.category_economy),
+	ENGLISH(11, R.string.category_english),
+	GEOGRAPHY(12, R.string.category_geography),
+	HISTORY(13, R.string.category_history),
+	COMPUTERSCIENCE(14, R.string.category_computerscience),
+	NETWORKSDATA(15, R.string.category_networksdata),
+	VIDEOGAMES(16, R.string.category_vieogames),
+	FOREIGNLANGUAGES(17, R.string.category_foreignlangages),
+	LITTERATURE(18, R.string.category_literature),
+	MATHS(19, R.string.category_maths),
+	MUSIC(20, R.string.category_music),
+	NATURE(21, R.string.category_nature),
+	PHOTOGRAPHY(22, R.string.category_photo),
+	RELIGION(23, R.string.category_religion),
+	MYTHOLOGY(24, R.string.category_mythology),
+	HEALTH(25, R.string.category_health),
+	SCIENCE(26, R.string.category_science),
+	SPORT(27, R.string.category_sport);
+
+	CategoriesEnum(int id, int name) {
+	}
+}
